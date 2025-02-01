45a05c253c2bb4ce00f90aed0131d87903e449b3
DESKTOP-EDCOMO0\Leonardo Pottmayer <leonardo.pottmayer@gmail.com>
2025-02-01 11:05:24 -0300

    Removed database scripts comments..

--- database/scripts/scripts.sql
+++ database/scripts/scripts.sql
@@ -1,22 +1,22 @@
 CREATE TABLE usr001_user (
-    id BIGSERIAL PRIMARY KEY, -- ID como chave primária e auto-incremento
-    name VARCHAR(255) NOT NULL, -- Nome do usuário
-    username VARCHAR(255) NOT NULL UNIQUE, -- Nome de usuário único
-    email VARCHAR(255) NOT NULL UNIQUE, -- E-mail único
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     role INTEGER NOT null,
-    password TEXT NOT NULL, -- Senha do usuário
-    password_salt TEXT NOT NULL -- Sal da senha
+    password TEXT NOT NULL,
+    password_salt TEXT NOT NULL
 );
 
 CREATE TABLE phr001_phrase (
-    id BIGSERIAL PRIMARY KEY,                 -- Identificador único da frase
-    description TEXT NOT NULL,             -- Descrição da frase
-    author TEXT NOT NULL,                  -- Autor da frase
+    id BIGSERIAL PRIMARY KEY,
+    description TEXT NOT NULL,
+    author TEXT NOT NULL,
     is_visible BOOLEAN NOT NULL DEFAULT true,
-    created_by BIGINT NOT NULL,            -- ID do usuário que criou o registro
-    created_at TIMESTAMP NOT NULL,         -- Data de criação do registro
-    updated_by BIGINT NOT NULL,            -- ID do usuário que atualizou o registro
-    updated_at TIMESTAMP NOT NULL,         -- Data da última atualização
+    created_by BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_by BIGINT NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_created_by FOREIGN KEY (created_by) REFERENCES usr001_user (id) ON DELETE RESTRICT,
     CONSTRAINT fk_updated_by FOREIGN KEY (updated_by) REFERENCES usr001_user (id) ON DELETE RESTRICT
 );
